8880c04ed022fa1f0d3ea8a306f336cbb3cf14ed
EricLoBin <ekilobin@gmail.com>
2022-06-08 15:15:06 -0300

    [UPDATE] Optimizations, youtube volume control

--- src/youtube/ad-skipper.js
+++ src/youtube/ad-skipper.js
@@ -1,12 +1,22 @@
 
-window.setInterval(function(){
-    if (!!document.querySelector(".ytp-ad-skip-button")){
-        document.querySelector(".ytp-ad-skip-button").click();
-    }
-    if (!!document.querySelector(".ytp-ad-overlay-close-button")){
-        document.querySelector(".ytp-ad-overlay-close-button").click();
-    }
-    if (!!document.querySelector("ytd-banner-promo-renderer:not(.dismissed) button")){
-        document.querySelector("ytd-banner-promo-renderer button").click();
-    }
-}, 500);
+(() => {
+    const observer = new MutationObserver(() => {
+        if (!!document.querySelector(".ytp-ad-skip-button")){
+            document.querySelector(".ytp-ad-skip-button").click();
+        }
+        if (!!document.querySelector(".ytp-ad-overlay-close-button")){
+            document.querySelector(".ytp-ad-overlay-close-button").click();
+        }
+        // Main page
+        if (!!document.querySelector("ytd-banner-promo-renderer:not(.dismissed) button")){
+            document.querySelector("ytd-banner-promo-renderer button").click();
+        }
+        if (!!document.getElementById("masthead-ad")){
+            document.getElementById("masthead-ad").remove();
+        }
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+})();
--- src/youtube/extra-config.js
+++ src/youtube/extra-config.js
@@ -1,92 +1,116 @@
-
-function createMenu() {
-    // Create the menu
-    let panel = document.createElement("div");
-    panel.id = "extra-config";
-    (() => {
-        // Create the menu header
-        let div = document.createElement("div");
-        div.className = "extra-config-header";
-        let p = document.createElement("p");
-        p.innerHTML = "Extra config";
-        div.appendChild(p);
-        
-        let expand_button = document.createElement("a");
-        expand_button.className = "expand-button";
-        expand_button.innerHTML = `
-        <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;">
-            <g class="style-scope yt-icon">
-                <polygon points="18.4,14.6 12,8.3 5.6,14.6 6.4,15.4 12,9.7 17.6,15.4 " class="style-scope yt-icon"></polygon>
-            </g>
-        </svg>`;
-        div.appendChild(expand_button);
-        panel.classList.add("hide");
-        div.onclick = () => {
-            if (panel.classList.contains("hide")) {
-                panel.classList.remove("hide");
-            } else {
-                panel.classList.add("hide");
-            }
-        };
-        panel.appendChild(div);
-    })();
-    
-    // Create the menu body
-    function createMenuItem(title, func) {
-        let item = document.createElement("div");
-        item.className = "menu-item";
-        let title_p = document.createElement("p");
-        title_p.innerHTML = title;
-        item.appendChild(title_p);
-        item.appendChild(func());
-        return item;
-    }
-    
-    (() => {
-        // playback speed option
-        let playbackSpeed = createMenuItem("Playback speed:", (() => {
-            let input = document.createElement("input");
-            input.type = "number";
-            input.min = "0.1";
-            input.step = "0.1";
-            input.value = sessionStorage.getItem("extra-config-playback-speed") || 1;
-            input.onchange = (e) => {
-                if (e.target.value < 0.1) e.target.value = 0.1;
-                sessionStorage.setItem("extra-config-playback-speed", e.target.value);
-                document.getElementsByTagName("video")[0].playbackRate = e.target.value; // playback Speed
-            };
-            return input;
-        }));
-
-        panel.appendChild(playbackSpeed);
-    })();
-    
-    let panels = document.querySelector("#secondary-inner #panels");
-    panels.insertBefore(panel, panels.firstChild);
-}
-
 (() => {
-    onUrlChange();
-    let lastUrl = location.href; 
-    new MutationObserver(() => {
-        const url = location.href;
-        if (url !== lastUrl) {
-            lastUrl = url;
-            onUrlChange();
-        }
-    }).observe(document, {subtree: true, childList: true});
+    // Audio context for volume control
+    let audioCtx = new AudioContext();
+    let source;
+    let gainNode;
+    // end Audio context for volume control
 
-    function onUrlChange() {
-        setTimeout(() => {
-            if (document.querySelector("#secondary-inner #panels")) {
-                if (window.location.href.includes("/watch")) {
-                    createMenu();
+    function createMenu() {
+        // Create the menu
+        let panel = document.createElement("div");
+        panel.id = "extra-config";
+        (() => {
+            // Create the menu header
+            let div = document.createElement("div");
+            div.className = "extra-config-header";
+            let p = document.createElement("p");
+            p.innerHTML = "Extra config";
+            div.appendChild(p);
+            
+            let expand_button = document.createElement("a");
+            expand_button.className = "expand-button";
+            expand_button.innerHTML = `
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;">
+                <g class="style-scope yt-icon">
+                    <polygon points="18.4,14.6 12,8.3 5.6,14.6 6.4,15.4 12,9.7 17.6,15.4 " class="style-scope yt-icon"></polygon>
+                </g>
+            </svg>`;
+            div.appendChild(expand_button);
+            panel.classList.add("hide");
+            div.onclick = () => {
+                if (panel.classList.contains("hide")) {
+                    panel.classList.remove("hide");
+                } else {
+                    panel.classList.add("hide");
                 }
-            } else {
-                onUrlChange();
-            }
-        }, 1000);
+            };
+            panel.appendChild(div);
+        })();
+        
+        // Create the menu body
+        function createMenuItem(title, func) {
+            let item = document.createElement("div");
+            item.className = "menu-item";
+            let title_p = document.createElement("p");
+            title_p.innerHTML = title;
+            item.appendChild(title_p);
+            item.appendChild(func());
+            return item;
+        }
+        
+        let video = document.getElementsByTagName("video")[0];
+        
+        (() => {
+            // playback speed option
+            let playbackSpeed = createMenuItem("Playback speed:", (() => {
+                let input = document.createElement("input");
+                input.type = "number";
+                input.min = "0.1";
+                input.step = "0.1";
+                input.value = sessionStorage.getItem("extra-config-playback-speed") || 1;
+                video.playbackRate = input.value;
+                input.onchange = (e) => {
+                    if (e.target.value < 0.1) e.target.value = 0.1;
+                    sessionStorage.setItem("extra-config-playback-speed", e.target.value);
+                    video.playbackRate = e.target.value; // playback Speed
+                };
+                return input;
+            }));
+            
+            panel.appendChild(playbackSpeed);
+            
+            // Volume option
+            let volume = createMenuItem(`Volume: 100%`, (() => {
+                let input = document.createElement("input");
+                input.type = "range";
+                input.min = "0";
+                input.max = "200";
+                input.value = 100;
+                
+                // Allow over 100%
+                if (!source) {
+                    source = audioCtx.createMediaElementSource(video);
+                    gainNode = audioCtx.createGain();
+                    source.connect(gainNode);
+                    gainNode.connect(audioCtx.destination);
+                }
+                gainNode.gain.value = 1;
+                
+                input.onchange = (e) => {
+                    gainNode.gain.value = e.target.value / 100; // volume
+                    volume.children[0].innerHTML = `Volume: ${e.target.value}%`;
+                };
+                return input;
+            }));
+            
+            panel.appendChild(volume);
+        })();
+        
+        let panels = document.querySelector("#secondary-inner #panels");
+        panels.insertBefore(panel, panels.firstChild);
     }
-    // parentElement.insertBefore(newElement, parentElement.children[2]);
-    //* document.getElementsByTagName("video")[0].playbackRate =  // playback Speed
-})()
+
+    // Add the menu
+    (() => {
+        const observer = new MutationObserver(() => {
+            if (!document.querySelector("video")) return;
+            if (!document.querySelector("#secondary-inner #panels")) return;
+            if (document.querySelector("#extra-config")) return;
+            createMenu();
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    })();
+})();
